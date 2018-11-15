f2ab15977b2271b326b962014d25f35938855f51
Robnarok <rokra102@hhu.de>
2018-11-15 14:48:29 +0100

    Ich habe voll nice die Attribute von dem AUto hinzugefügt.

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,17 @@
 package de.hhu.propra.db.entities;
 
-public class Auto{
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Auto{
+    @Id
+    private long id;
+
+    private String marke;
+    private String farbe;
+    private String hubraum;
 }
